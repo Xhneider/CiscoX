9e86fa0c3da051b3aeb16c5e56635c7fbf44d208
Xhneider <Xhneider@gmail.com>
2023-09-15 22:27:32 -0600

    15_09_23 Se arreglo el bug del boton Ad

--- components/BotonAd.js
+++ components/BotonAd.js
@@ -1,75 +1,79 @@
-import React, {useEffect, useState} from 'react';
-import {TouchableWithoutFeedback, Text, View, StyleSheet} from 'react-native';
-import {InterstitialAd, AdEventType} from 'react-native-google-mobile-ads';
-import Icon from 'react-native-vector-icons/MaterialIcons'
+import React, { useEffect, useState } from 'react';
+import { TouchableWithoutFeedback, Text, View, StyleSheet } from 'react-native';
+import { InterstitialAd, AdEventType } from 'react-native-google-mobile-ads';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function BotonAd() {
-  const [loaded, setLoaded] = useState(false);
   const [interstitial, setInterstitial] = useState(null);
-  const [Gracias, setGracias] = useState(false);
+  const [gracias, setGracias] = useState(false);
 
   useEffect(() => {
-    // cambiar a hacer el apk
     const interstitialAd = InterstitialAd.createForAdRequest(
-      'ca-app-pub-3940256099942544/1033173712',
+      'ca-app-pub-3940256099942544/1033173712', // Reemplaza con tu ID de anuncio
     );
 
     const loadInterstitial = async () => {
       try {
         await interstitialAd.load();
-        setLoaded(true);
       } catch (error) {
-        console.error('Error al cargar anuncio', error);
-        setLoaded(false);
+        // ERROR AL CARGAR EL ANUNCIO
+        console.log('Error al cargar anuncio');
       }
     };
 
     interstitialAd.addAdEventListener(AdEventType.CLOSED, () => {
-      // El anuncio se ha cerrado, por lo que podemos cargarlo nuevamente.
       loadInterstitial();
       setGracias(true);
     });
-    loadInterstitial();
 
+    loadInterstitial();
     setInterstitial(interstitialAd);
   }, []);
 
+  //Funcion para el evento del boton
+  const showInterstitial = async () => {
+    try {
+      if (interstitial) {
+        await interstitial.show();
+      } else {
+        console.error('El anuncio no se ha cargado correctamente.');
+      }
+    } catch (error) {
+      //ERROR AL CARGAR EL ANUNCIO
+      console.log('Error al mostrar anuncio');
+    }
+  };
+
   return (
     <>
-      <TouchableWithoutFeedback
-        onPress={() => {
-          if (interstitial) {
-            interstitial.show();
-          }
-        }}>
+      <TouchableWithoutFeedback onPress={showInterstitial}>
         <View style={style.btonAd}>
-        <Icon name={'coffee'} size={20} />
-          <Text style={{fontFamily:'MPLUSRounded1c-Black',paddingLeft:10}}>Invitame A Un Café</Text>
+          <Icon name={'coffee'} size={20} />
+          <Text style={{ fontFamily: 'MPLUSRounded1c-Black', paddingLeft: 10 }}>
+            Invítame A Un Café
+          </Text>
         </View>
       </TouchableWithoutFeedback>
 
-      {Gracias ? (
+      {gracias ? (
         <View>
           <Text>Muchas Gracias</Text>
         </View>
-      ) : (
-        <View>
-        </View>
-      )}
+      ) : null}
     </>
   );
 }
 
-const style=StyleSheet.create({
-  btonAd:{
-    height:40,
-    width:200,
-    backgroundColor:'#2271b3',
-    borderRadius:7,
-    display:'flex',
-    alignItems:'center',
-    justifyContent:'center',
-    flexDirection:'row',
-    marginBottom:10
-  }
-})
+const style = StyleSheet.create({
+  btonAd: {
+    height: 40,
+    width: 200,
+    backgroundColor: '#2271b3',
+    borderRadius: 7,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+});
