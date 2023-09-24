8f1a572fb10760185333e578ee1b3ccced0c9356
Xhneider <Xhneider@gmail.com>
2023-09-23 22:39:59 -0600

    23/09/23 Se agrego informacion

--- components/ComanDataScreen.js
+++ components/ComanDataScreen.js
@@ -1,21 +1,67 @@
-import { View, Text,StyleSheet} from 'react-native'
-import React from 'react'
+import {View, Text, StyleSheet} from 'react-native';
+import React, {useContext} from 'react';
+import comandosDeConfiguracion from './BDComandos';
+import {Dispositivo} from './ContextoApp';
 
 const ComanDataScreen = ({route}) => {
+  const {dispositivo} = useContext(Dispositivo);
+  let id = route.params.id;
   return (
-    <View>
-      <Text style={styles.text}>{route.params.id}</Text>
+  
+    <View style={styles.fondo}>
+      {/* DATOS de comandos */}
+      <View style={styles.contendorComandos}>
+        <Text style={styles.textComandos}>
+          {comandosDeConfiguracion[dispositivo][id].comando}
+        </Text>
+      </View>
+        {/* DATOS de procedimiento */}
+      <View>
+        <Text style={styles.textPasos}>
+          {comandosDeConfiguracion[dispositivo][id].pasos}
+        </Text>
+      </View>
+        {/* DATOS de Concepto */}
+      <View>
+        <Text style={styles.textPasos}>
+          {comandosDeConfiguracion[dispositivo][id].concepto}
+        </Text>
+      </View>
+
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  text:{
-    fontSize:25,
-    color:'black',
-    fontFamily:'MPLUSRounded1c-Light'
-},
+  contendorComandos:{
+    margin:18,
+    marginTop:50,
+    backgroundColor:'#fff',
+    padding:'20',
+    height:200,
+    display:'flex',
+    justifyContent:'flex-end',
+    borderRadius:10
 
-})
+  },
+  textComandos: {
+    padding:10,
+    fontSize: 17,
+    color: 'black',
+    fontFamily: 'CourierPrime-Bold',
+  },
+  textPasos: {
+    padding:10,
+    fontSize: 17,
+    color: '#fff',
+    fontFamily: 'MPLUSRounded1c-Medium',
+  },
+  fondo:{
+    width:'100%',
+    height:'100%',
+    backgroundColor:'#191924',
+    position:'relative'
+  },
+});
 
-export default ComanDataScreen
+export default ComanDataScreen;
